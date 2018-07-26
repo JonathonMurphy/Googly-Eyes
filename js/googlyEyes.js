c23dc50dcb7fc73c7b51b3c379c346f06d7f9955
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2018-07-26 14:33:15 -0400

    Update 4: Groundwork for object creation

--- js/googlyEyes.js
+++ js/googlyEyes.js
@@ -1,76 +1,10 @@
 $ (function(){
-	const leftPupil = $('#leftPupil');
-	const rightPupil = $('#rightPupil');
-
-	//Speed slider for animation
-
-	/* Not working due to how css animation speeds are set
-
-	let speed = 200;
-	console.log(speed);
-
-	$('#speed').change(function(){
-		speed = $(this).val();
-		console.log(speed);
-		return speed;
-	});
-
-	*/
-
-	/*
-Speed change is not effecting the duration of the eye fall animation.
-the speed is being loaded in to the CSS at page load( not on page load, but the first time the function fires), but not being reloaded when it changes.
-Instead the duration changes to the default duration amount.
-Left Eye = Variable
-Right Eye = Control
-	*/
-
-//Google Eye Effect
-	function leftEyeFall() {
-		let leftPupilPosition = leftPupil.position().top;
-		let fallDistance = 257.5 - leftPupilPosition + 'px';
-		if (leftPupil.position().top < 257.5){
-			leftPupil.animate({
-					top: '+='+fallDistance
-			}, speed);
-		}
-	}
-	leftEyeFall();
-	leftPupil.draggable({
-		containment: 'parent'
-	}).mouseup(function() {
-		leftEyeFall();
-	});
-
-
-
-	function rightEyeFall() {
-		let rightPupilPosition = rightPupil.position().top;
-		let fallDistance = 257.5 - rightPupilPosition + 'px';
-		if (rightPupil.position().top < 257.5){
-			rightPupil.animate({
-					top: '+='+fallDistance
-			}, 400);
-		}
-	}
-	rightEyeFall();
-	rightPupil.draggable({
-		containment: 'parent'
-	}).mouseup(function() {
-		rightEyeFall();
-	});
-
-	// Googly Eye Object
-
 	let googlyEye = {
-
 		//Establishing variables
 		eye: 'some stuff, maybe a unique ID',
 		pupil: 'other stuff, prolly also a unique ID',
 		eyeID#: 0,
 		pupilID#: 0,
-
-
 		//Create new eye and pupil function
 		createGooglyEyeHTML: function() {
 			newEyeID = eye;
@@ -79,8 +13,6 @@ Right Eye = Control
 			newPupilID += this.pupilID++;
 			$('.face').append("<div class='eye' id='"newEyeID"'><div class='pupil ' id='"newPupilID"'></div></div>")
 		},
-
-
 		//Pupil animation
 		eyeFall: function() {
 			let pupilPosition = this.pupil.position().top;
@@ -91,7 +23,6 @@ Right Eye = Control
 				}, speed);
 			}
 		},
-
 		//Pupil drag function
 		eyeDrag: function() {
 			this.pupil.draggable({
@@ -100,33 +31,5 @@ Right Eye = Control
 				eyeFall();
 			});
 		}
-
 	}
-
-//Randomly changing background color (RGB Color)
-/*
-	let rgb;
-
-	function randomByte() {
-		return Math.floor(Math.random() * 256);
-	}
-
-	function randomColorGenerator() {
-		rgb = 'rgb(' + randomByte() + ',' + randomByte() + ',' + randomByte() + ')';
-		return rgb;
-	}
-
-	randomColorGenerator();
-
-	function backgroundColorLoop() {
-		$('body').animate({
-			backgroundColor: rgb
-		}, speed, function(){
-			randomColorGenerator();
-			backgroundColorLoop();
-		});
-	}
-
-	backgroundColorLoop();
-	*/
 });
