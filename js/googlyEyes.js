ac53501e153dab67399c1035e8cbf65e9f6fdab3
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2018-07-27 14:38:28 -0400

    Update: 6

--- js/googlyEyes.js
+++ js/googlyEyes.js
@@ -1,50 +1,57 @@
 $ (function(){
-
 	let clickArea = $('.click-area');
 	clickArea.css('height', $(window).height());
-
 	let googlyEye = {
 		//Establishing variables
 		eyeID: 0,
 		pupilID: 0,
 		//Create new eye and pupil function
-		createGooglyEyeHTML: function() {
+		createGooglyEyeHTML: function(x, y) {
 			newEyeID = 'eye';
 			newEyeID += this.eyeID++;
 			newPupilID = 'pupil';
 			newPupilID += this.pupilID++;
 			clickArea.append("<div class='eye' id='" + newEyeID + "'><div class='pupil ' id='" + newPupilID + "'></div></div>");
-
-			$("#" + newEyeID).css({
-				'top': 100,
-				'left': 200
-			})
-
-			console.log('Object Creation Works!');
-
+			//Set Eye Position
+			$('.eye').last().css({
+				'left': x,
+				'top': y
+			});
+			this.eyeFall();
+			console.log('#' + newPupilID);
 		},
 		//Pupil animation
 		eyeFall: function() {
-			let pupilPosition = this.pupil.position().top;
-			let fallDistance = 257.5 - pupilPosition + 'px';
-			if (this.pupil.position().top < 257.5){
-				this.pupil.animate({
+			let pupilPosition = $('#' + newPupilID).position().top;
+			let fallDistance = 40 - pupilPosition + 'px';
+			if (pupilPosition < 40){
+				$('#' + newPupilID).animate({
 						top: '+='+fallDistance
-				}, speed);
+				}, 200);
 			}
 		},
 		//Pupil drag function
 		eyeDrag: function() {
-			this.pupil.draggable({
+			$('#' + newPupilID).draggable({
 				containment: 'parent'
 			}).mouseup(function() {
-				eyeFall();
+				this.eyeFall();
 			});
 		}
 	}
 //Add new googlyEye object on right click
 	clickArea.dblclick(function() {
-		googlyEye.createGooglyEyeHTML();
+		let x = event.pageX;
+		let y = event.pageY;
+		googlyEye.createGooglyEyeHTML(x, y);
+	});
+
+	clickArea.mousedown(function() {
+		googlyEye.eyeDrag();
+	});
+
+	clickArea.mouseup(function() {
+		googlyEye.eyeFall();
 	});
 
 });
