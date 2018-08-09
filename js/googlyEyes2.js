be410594888a4b8a1cbfacf91c539dd2f924d299
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2018-08-09 13:42:44 -0400

    Update 8

--- js/googlyEyes2.js
+++ js/googlyEyes2.js
@@ -3,21 +3,16 @@ $ (function(){
 	let clickArea = $('.click-area');
 	clickArea.css('height', $(window).height());
 	let i = 0;
-	let googlyEyeArray =[];
+	let googlyEyeArray = [];
+
 
 	//Beginning of object
 	function googlyEye(i, x, y, html) {
 		this.x = event.pageX;
 		this.y = event.pageY;
-		this.html = "<div class='eye' id='eye"+i+"' "
-		this.html += "style='top:"+this.y+"px; left:"+this.x+"px;'>"
-		this.html += "<div class='pupil ' id='pupil"+i+"'></div></div>";
-
-		this.movePupil = function () {
-			$("'#pupil"+i+"'").draggable({
-			containment: 'parent'})
-		};
-
+		this.html = "<div class='eye' id='eye"+i+"' ";
+		this.html += "style='top:"+this.y+"px; left:"+this.x+"px;'>";
+		this.html += "<div class='pupil ' id='pupil"+i+"'></div></div>"
 	} // End of object
 
 
@@ -26,11 +21,12 @@ $ (function(){
 
 	clickArea.dblclick(function() {
 		googlyEyeArray.push(i);
-		googlyEyeArray[i] = new googlyEye();
+		googlyEyeArray[i] = new googlyEye(i);
 		clickArea.append(googlyEyeArray[i].html);
-		googlyEye[i].movePupil();
-		i++;
+		$('#pupil'+i).draggable({
+			containment: 'parent'});
 		console.log(googlyEyeArray);
+		i++;
 	});
 
 });
