c4e09159b15978799a1e194e82866e04ab0fa81c
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2018-08-10 08:58:15 -0400

    Update 9

--- js/googlyEyes2.js
+++ js/googlyEyes2.js
@@ -1,35 +1,52 @@
 $ (function(){
 
+	let main;
+	let i = 0;
+	let radius = 10;
+	let googlyEyeArray = [];
 	let clickArea = $('.click-area');
 	clickArea.css('height', $(window).height());
-	let i = 0;
-	let googlyEyeArray = [];
 
-
-	//Beginning of object
+	//Googly Eye Object
 	function googlyEye(i, x, y, html) {
 		this.x = event.pageX;
 		this.y = event.pageY;
 		this.html = "<div class='eye' id='eye"+i+"' ";
 		this.html += "style='top:"+this.y+"px; left:"+this.x+"px;'>";
 		this.html += "<div class='pupil ' id='pupil"+i+"'></div></div>"
-	} // End of object
+	}
 
-
-
-//Add new googlyEye object on double click
-
-	clickArea.dblclick(function() {
+	//Event Triggers
+	clickArea.mouseup(function() {
+		mainLoop(i);
+	}).dblclick(function() {
 		googlyEyeArray.push(i);
 		googlyEyeArray[i] = new googlyEye(i);
 		clickArea.append(googlyEyeArray[i].html);
 		$('#pupil'+i).draggable({
-			containment: 'parent'});
-		console.log(googlyEyeArray);
+			containment: 'parent',
+			//Thanks to Borgboy on stackOverflow
+			drag: function( event, ui ) {
+	        var x = ui.position.left - radius,
+	            y = radius - ui.position.top,
+	            h = Math.sqrt(x*x + y*y);
+	        if (Math.floor(h) > radius) {
+	            ui.position.top = radius - Math.round(radius * y / h);
+	            ui.position.left = Math.round(radius * x / h) + radius;
+	        }}});
 		i++;
+		mainLoop(i);
 	});
 
+//Main animation loop
+	function mainLoop(i) {
+		for (x = 0; x < i; x++) {
+			let pupilPosition = $('#pupil' + x).position().top;
+			let fallDistance = 20 - pupilPosition + 'px';
+			if (pupilPosition < 20){
+				$('#pupil' + x).animate({
+						top: '+='+fallDistance
+				}, 200);
+			}}}
+
 });
-
-
-// move the positioning function to the dbl click, and bring the x and y variables with it
