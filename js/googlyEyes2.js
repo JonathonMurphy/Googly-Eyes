ef9d07c7f596a08ded0f575f4074d7f6f9bb1163
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2018-08-10 13:06:46 -0400

    Update 10

--- js/googlyEyes2.js
+++ js/googlyEyes2.js
@@ -1,13 +1,32 @@
 $ (function(){
 
+	//Les Variables
 	let main;
 	let i = 0;
-	let radius = 10;
 	let googlyEyeArray = [];
-	let clickArea = $('.click-area');
-	clickArea.css('height', $(window).height());
+	let $clickArea = $('.click-area');
+	let $sliderValue = $('#slider').val();
+	let eyeSize = $sliderValue;
+	let pupilSize = $sliderValue * 0.4;
+	let pupilLeft = $sliderValue * 0.3;
+	let radius = $sliderValue * 0.28;
+	let sizeDifference = eyeSize - pupilSize;
+	$clickArea.css('height', $(window).height());
 
-	//Googly Eye Object
+
+	//Update Slider Value
+	$('.slider').change(function() {
+		$sliderValue = $('#slider').val();
+		eyeSize = $sliderValue;
+		pupilSize = $sliderValue * 0.4;
+		pupilLeft = $sliderValue * 0.3;
+		radius = $sliderValue * 0.28;
+		sizeDifference = eyeSize - pupilSize;
+		return $sliderValue, eyeSize, pupilSize, pupilLeft, radius, sizeDifference;
+	})
+
+
+	//Googly Eye Object Constructor
 	function googlyEye(i, x, y, html) {
 		this.x = event.pageX;
 		this.y = event.pageY;
@@ -16,13 +35,20 @@ $ (function(){
 		this.html += "<div class='pupil ' id='pupil"+i+"'></div></div>"
 	}
 
-	//Event Triggers
-	clickArea.mouseup(function() {
+
+	//Event Triggers & Parameters
+	$clickArea.mouseup(function() {
 		mainLoop(i);
 	}).dblclick(function() {
 		googlyEyeArray.push(i);
 		googlyEyeArray[i] = new googlyEye(i);
-		clickArea.append(googlyEyeArray[i].html);
+		$clickArea.append(googlyEyeArray[i].html);
+		$('#eye'+i).draggable({
+			containment: 'parent'}).css({
+				'width' : eyeSize + 'px',
+				'height' : eyeSize + 'px',
+				'border-radius' : eyeSize + 'px'
+			});
 		$('#pupil'+i).draggable({
 			containment: 'parent',
 			//Thanks to Borgboy on stackOverflow
@@ -33,17 +59,23 @@ $ (function(){
 	        if (Math.floor(h) > radius) {
 	            ui.position.top = radius - Math.round(radius * y / h);
 	            ui.position.left = Math.round(radius * x / h) + radius;
-	        }}});
+	        }}}).css({
+						'width' : pupilSize + 'px',
+						'height' : pupilSize + 'px',
+						'border-radius' : pupilSize + 'px',
+						'left' : pupilLeft + 'px'
+					});
 		i++;
 		mainLoop(i);
 	});
+
 
 //Main animation loop
 	function mainLoop(i) {
 		for (x = 0; x < i; x++) {
 			let pupilPosition = $('#pupil' + x).position().top;
-			let fallDistance = 20 - pupilPosition + 'px';
-			if (pupilPosition < 20){
+			let fallDistance = sizeDifference - pupilPosition + 'px';
+			if (pupilPosition < 15){
 				$('#pupil' + x).animate({
 						top: '+='+fallDistance
 				}, 200);
